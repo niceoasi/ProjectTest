8bc39efe88c11d947482df71a57851e5a1551dac
Daeyun Ethan Kim <nicebuvr@naver.com>
2018-11-10 15:50:58 +0900

    Initial Commit

---
+++ ProjectTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjectTest
+//
+//  Created by Daeyun Ethan Kim on 10/11/2018.
+//  Copyright © 2018 K.D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
